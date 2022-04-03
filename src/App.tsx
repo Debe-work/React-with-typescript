4756c52a359900270ee784829060d9c22c3f8fcc
Debe-work <80156173+Debe-work@users.noreply.github.com>
2022-04-04 07:49:57 +0900

    addC

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { Canvas } from "@react-three/fiber";
 import "./styles.css";
 
 const AGeometory = () => {
-  return <planeBufferGeometry />;
+  return <boxBufferGeometry args={[10, 10, 10]} />;
 };
 
 const BGeometory = () => {
-  return <sphereBufferGeometry />;
+  return <sphereBufferGeometry args={[10, 10, 10]} />;
+  // return <sphereBufferGeometry args={[10, 10, 10]} />;
+};
+
+const CGeometory = () => {
+  return <sphereBufferGeometry args={[10, 10, 10]} />;
 };
 
 export default function App() {
   const SwitchedMaterial = () => {
-    const geometryType = "A";
+    const geometryType = "B";
     switch (geometryType) {
       case "A": {
         return <AGeometory />;
@@ -26,7 +31,16 @@ export default function App() {
   };
   // const geometryType = "B";
   return (
-    <Canvas>
+    <Canvas
+    // camera={{
+    //   position: [100, 100, 100], // camera position x, y, z (*)
+    //   fov: 50, // field of view : 視野角 (*)
+    //   aspect: window.innerWidth / window.innerHeight, // アス比
+    //   near: -100, // 描画する最近接位置
+    //   far: 100 // 描画する最遠方位置
+    // }}
+    >
+      {/* <mesh position={[-10, -10, -10]}> */}
       <mesh>
         <SwitchedMaterial />
       </mesh>
